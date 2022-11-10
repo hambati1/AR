ebe5398a83cd76e5393a8db46a3e64971c19271b
praveen <hambati@gmail.com>
2022-11-11 02:08:03 +0530

    API call implementation ...

--- src/@crema/core/AppLayout/DrawerLayout/AppHeader.js
+++ src/@crema/core/AppLayout/DrawerLayout/AppHeader.js
@@ -16,49 +16,7 @@ const AppHeader = ({showDrawer}) => {
 
   return (
     <div className={styles.appDrawerLayoutHeader}>
-      <a className={styles.trigger} onClick={showDrawer}>
-        <AiOutlineMenu />
-      </a>
-      <AppLogo />
-      <div className='flex-grow-1' />
-      <div className='me-2 me-md-4'>
-        <AppSearch />
-      </div>
-      <div className='d-none d-md-flex mx-n2'>
-       
-        <span className='px-2'>
-          <AppNotifications />
-        </span>
-        <span className='px-2'>
-          <AppHeaderMessages />
-        </span>
-      </div>
-      <div className='d-md-none'>
-        <Dropdown>
-          <Dropdown.Toggle
-            id='dropdown-custom-components'
-            className={clsx(
-              styles.dropdownToggleBtn,
-              'p-0 bg-transparent border-0 d-flex align-items-center w-100 shadow-none',
-            )}>
-            <AppIconBtn>
-              <FiMoreVertical />
-            </AppIconBtn>
-          </Dropdown.Toggle>
-
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">
-              <AppHeaderMessages />
-            </Dropdown.Item>
-            <Dropdown.Item href="#/action-2">
-              <AppNotifications />
-            </Dropdown.Item>
-            <Dropdown.Item href="#/action-3">
-              <AppLanguageSwitcher />
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
+     header
     </div>
   );
 };
--- src/pages/errorPages/Error401/index.js
+++ src/pages/errorPages/Error401/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import IntlMessages from '@crema/utility/IntlMessages';
 import AppAnimateGroup from '@crema/core/AppAnimateGroup';
 import '../index.style.scss';
 import AppPageMetadata from '@crema/core/AppPageMetadata';
 import Button from 'devextreme-react/button';
-// import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 import '../../errorPages/Error401/index.style.scss'
 import DataGrid, {
   Column,
@@ -16,18 +16,15 @@ import DataGrid, {
   FilterRow,
   Toolbar
 } from 'devextreme-react/data-grid';
-// import 'devextreme/dist/css/dx.generic.ATD-DataGrid-md - Copy1.css';
 import DropDownButton from 'devextreme-react/drop-down-button';
 import style from '../Error401/index.style.scss';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
 import { Search } from 'react-bootstrap-icons';
-
+import {onPaymentList} from '../../../redux/actions/paymentList';
+import {onGetContactList} from '../../../redux/actions/ContactApp';
 
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
-
-
-// import { Paper } from '@material-ui/core';
 
 const actions = [
   { id: 1, text: "File Name" },
@@ -57,7 +54,6 @@ export const NonCitizenDetails = [
     recordsimported: '3',
     recordsinerror: '0',
     amountimported: '$40.14',
-
   },
   {
     filename: 'pngpay.remit.20221018011236',
@@ -65,17 +61,79 @@ export const NonCitizenDetails = [
     recordsimported: '23',
     recordsinerror: '2',
     amountimported: '$488.47',
-
-  },
-
+  }
 ];
 
-
 const NonCitizen = () => {
+    const [searchType, setSearchTypes] = useState();
+    const [fileType, setFileTypes] = useState();
+    const [selectfileType, setselectfileType] = useState("")
+    const [brand, setBrand] = useState("")
+    const [selectedFile, setSelectedFile] = useState();
+    const [file, setFile] = useState();
+    const path='http://172.20.51.231:8761/cm/api';
+    const session="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJoYW1iYXRpIiwic2NvcGVzIjpbIlJFRlJFU0hfVE9LRU4iXSwiaXNzIjoiUE5HIiwiaWF0IjoxNjY4MDgwOTQ5LCJleHAiOjE2Njg2ODA5NDl9.pGLmio8UUaAIg0gDZ0ufMYg2STgxs0SYiA-DBWi4OUYlSYOhToRWxAu2jt1JiKmD4wxuqLGfARky4mSdX-qYpA";
+    const b0={"custId": 10040003,"importFileId": 10053782,"batchId": 10043555,"page": 1,"size": 3};
+
+ const inputChangeHandler = (setFunction: React.Dispatch<React.SetStateAction<string>>, event: React.ChangeEvent<HTMLInputElement>) => {
+        setFunction(event.target.value)
+    }
+     const selectChangeHandler = (setFunction: React.Dispatch<React.SetStateAction<string>>, event: React.ChangeEvent<HTMLInputElement>) => {
+            console.log(event);
+            setselectfileType(event);
+        }
+
+const handleChange = (event) => {
+    setFileTypes(event.target.value)
+  }
+
+const changeHandler=(event)=>{
+		setFile(event.target.files[0]);
+	};
+
+    const getSearchData = async () => {
+    const response = await fetch(path+"/ar/searchworklist",{ method: 'POST',
+       headers: {"Content-Type":'application/json',Session: session},
+       body:JSON.stringify(b0)}
+    ).then((response) => response.json());
+console.log(response.response);
+    setSearchTypes(response.response);
+  };
+
+const getFileTypeData = async () => {
+    const response = await fetch(path+"/cn/filetype?functionId=6&isActive=1&isImport=1",{ method: 'GET',
+       headers: {Session: session}}
+    ).then((response) => response.json());
+console.log(response.response);
+    setFileTypes(response.response);
+  };
+
+  useEffect(() => {
+    getSearchData();
+    getFileTypeData();
+  }, []);
+
+   const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+         const form=event.target;
+         event.preventDefault();
+     const url = path+'/import/file';
+    const formData = new FormData();
+    formData.append('fileNames', file);
+     formData.append('brandId', 1);
+      formData.append('fileTypeId', selectfileType);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+        Session: session
+      },
+    };
+    axios.post(url, formData, config).then((response) => {
+      console.log(response.data);
+    });
+
+    }
 
   return (
-    // <Paper>
-    //   <Box>
     <div >
       <div className='col-md-9 main-header'>
         <p>Accounts Receivable</p>
@@ -84,23 +142,41 @@ const NonCitizen = () => {
       <Tabs
         defaultActiveKey="profile"
         id="uncontrolled-tab-example"
-        className="mb-3"
-      >
+        className="mb-3">
         <Tab eventKey="home" title="Search">
+         <div class="form-group">
+           <div class="mb-3 row">
+                        <label for="inputFileType" class="col-lg-1 col-form-label">Search Type :</label>
+ <div class="col-sm-5 Dropdown">
+                         <select class="form-select" name="searchType" aria-label="Default select example">
+                              {searchType &&
+                                searchType.map((user) => (
+                                 <option value="{user.fileTypeId}">{user.fileTypeDesc}</option>
+                                ))}
+                         </select>
+                         </div>
 
-
-
+                         <div className="padding">
+                                         <button type="submit" class="btn  mb-3 btn-Gray ">Submit</button>
+                                         <button type="reset" value="Reset" class="btn  mb-3 btn-darkGray ">Reset</button>
+                         </div>
+           </div>
+           </div>
         </Tab>
+
         <Tab eventKey="profile" title="File Import">
-
-          <div>
-
-            <form>
+         <div class="form-group">
+            <form onSubmit={onSubmitHandler}>
               <div class="mb-3 row">
                 <label for="inputFileType" class="col-lg-1 col-form-label">File Type</label>
 
                 <div class="col-sm-5 Dropdown">
-                  <input type="filetype" className='form-control Dropdown' id="inputFiletype" />
+                   <select class="form-select" name="selectfileType" aria-label="Default select example" onChange={(e)=>selectChangeHandler(selectfileType,e.target.value)}>
+                                               {fileType &&
+                                                 fileType.map((user) => (
+                                                  <option value={user.fileTypeId}>{user.fileTypeDesc}</option>
+                                                 ))}
+                                          </select>
                 </div>
               </div>
 
@@ -109,28 +185,23 @@ const NonCitizen = () => {
                 <label for="inputBrand" class="col-lg-1 col-form-label">Brand</label>
                 {/* <input type="" class="form-control" id="input" placeholder="PNG" /> */}
                 <div class="col-sm-5">
-                  <input type="Brand" class="form-control" id="inputBrand" />
+                  <input type="Brand" name="brand" class="form-control" id="inputBrand"  onChange={(e)=>inputChangeHandler(setBrand, e)} />
                 </div>
               </div>
-
 
               <div class="row g-3">
                 <div class="col-lg-1">
-                  {/* <label for="" class="visually-hidden">File Name</label> */}
                   <label for="Filename" class="col-sm-10 col-form-label">File Name</label>
-                  {/* <input type="text" readonly class="form-control-plaintext" id="" value="File Name" /> */}
                 </div>
                 <div class="col-auto">
                   <label for="" class="visually-hidden"></label>
-                  <input type="" class="form-control" id="input" placeholder="" />
+                  <input type="file" name="file" class="form-control" id="input" placeholder="" onChange={changeHandler}  />
                 </div>
-                <div class="col-auto">
-                  <button type="submit" class="btn mb-3 btn-darkGray ">Browse</button>
-                </div>
+
               </div>
               <div>
                 <button type="submit" class="btn  mb-3 btn-Gray ">Import</button>
-                <button type="submit" class="btn  mb-3 btn-darkGray ">Clear</button>
+                <button type="reset"  class="btn  mb-3 btn-darkGray ">Clear</button>
               </div>
             </form>
           </div>
@@ -159,17 +230,10 @@ const NonCitizen = () => {
             />
             <Pager allowedPageSizes={[5, 10, 20]} showPageSizeSelector={true} showNavigationButtons={true} />
             <Paging defaultPageSize={5} />
-
           </DataGrid>
-
         </Tab>
-
       </Tabs>
     </div>
-
-    //    </Box>
-    // </Paper>
-
   );
 };
 
