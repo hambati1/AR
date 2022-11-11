7fc2b97a80fc28e6cccc03f4bef024205db946ad
praveen <hambati@gmail.com>
2022-11-11 15:36:02 +0530

    payment file added

---
+++ src/redux/actions/paymentList.js
@@ -0,0 +1,38 @@
+import {
+  FETCH_ERROR,
+  FETCH_START,
+  FETCH_SUCCESS,
+  GET_USER_LIST,
+} from '../../shared/constants/ActionTypes';
+import {appIntl} from '@crema/utility/helper/Utils';
+import jwtAxios from '@crema/services/auth/jwt-auth';
+
+export const onPaymentList = () => {
+
+const getApiData = async () => {
+    const response = await fetch(
+      "http://172.20.51.231:8761/cm/api/cn/filetype?functionId=6&isActive=1&isImport=1"
+    ).then((response) => response.json());
+
+    setUsers(response);
+  };
+
+  return (dispatch) => {
+    jwtAxios
+      .get('/api/user/list88888888888888888888888888888888')
+      .then((data) => {
+        if (data.status === 200) {
+          dispatch({type: FETCH_SUCCESS});
+          dispatch({type: GET_USER_LIST, payload: data.data});
+        } else {
+          dispatch({
+            type: FETCH_ERROR,
+            payload: messages['message.somethingWentWrong'],
+          });
+        }
+      })
+      .catch((error) => {
+        dispatch({type: FETCH_ERROR, payload: error.message});
+      });
+  };
+};
